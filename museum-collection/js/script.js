55a3d95536fcbb8ae7f40b5c0a1d63648db584a0
Rylee094 <itsrylee151@gmail.com>
2025-10-29 21:33:11 -0500

    add js to code

---
+++ museum-collection/js/script.js
@@ -0,0 +1,359 @@
+
+/*BUTTON 1*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button1');
+  const Line1 = document.getElementById('Line1');
+
+  fadeButton.addEventListener('click', function() {
+    Line1.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button2.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 2*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button2');
+  const myElement = document.getElementById('Line2');
+
+  fadeButton.addEventListener('click', function() {
+    Line2.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button2.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button3.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 3*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button3');
+  const myElement = document.getElementById('Line3');
+
+  fadeButton.addEventListener('click', function() {
+    Line3.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button3.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button4.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 4*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button4');
+  const myElement = document.getElementById('Line4');
+
+  fadeButton.addEventListener('click', function() {
+    Line4.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button4.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button5.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 5*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button5');
+  const myElement = document.getElementById('Line5');
+
+  fadeButton.addEventListener('click', function() {
+    Line5.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button5.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button6.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 6*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button6');
+  const myElement = document.getElementById('Line6');
+
+  fadeButton.addEventListener('click', function() {
+    Line6.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button6.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button7.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 7*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button7');
+  const myElement = document.getElementById('Line7');
+
+  fadeButton.addEventListener('click', function() {
+    Line7.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button7.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button8.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 8*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button8');
+  const myElement = document.getElementById('Line8');
+
+  fadeButton.addEventListener('click', function() {
+    Line8.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button8.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button9.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 9*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button9');
+  const myElement = document.getElementById('Line9');
+
+  fadeButton.addEventListener('click', function() {
+    Line9.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button9.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button10.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 10*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button10');
+  const myElement = document.getElementById('Line10');
+
+  fadeButton.addEventListener('click', function() {
+    Line10.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button10.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button11.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 11*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button11');
+  const myElement = document.getElementById('Line11');
+
+  fadeButton.addEventListener('click', function() {
+    Line11.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button11.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button12.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 12*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button12');
+  const myElement = document.getElementById('Line12');
+
+  fadeButton.addEventListener('click', function() {
+    Line12.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button12.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button13.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 13*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button13');
+  const myElement = document.getElementById('Line13');
+
+  fadeButton.addEventListener('click', function() {
+    Line13.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button13.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button14.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 14*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button14');
+  const myElement = document.getElementById('Line14');
+
+  fadeButton.addEventListener('click', function() {
+    Line14.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button14.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button15.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 15*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button15');
+  const myElement = document.getElementById('Line15');
+
+  fadeButton.addEventListener('click', function() {
+    Line15.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button15.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button16.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 16*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button16');
+  const myElement = document.getElementById('Line16');
+
+  fadeButton.addEventListener('click', function() {
+    Line16.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button16.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button17.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 17*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button17');
+  const myElement = document.getElementById('Line17');
+
+  fadeButton.addEventListener('click', function() {
+    Line17.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button17.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button18.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 18*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button18');
+  const myElement = document.getElementById('Line18');
+
+  fadeButton.addEventListener('click', function() {
+    Line18.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button18.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button19.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 19*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button19');
+  const myElement = document.getElementById('Line19');
+
+  fadeButton.addEventListener('click', function() {
+    Line19.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button19.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button20.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 20*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button20');
+  const myElement = document.getElementById('Line20');
+
+  fadeButton.addEventListener('click', function() {
+    Line20.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button20.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button21.style.display = 'block'; 
+        });
+});
+
+/*BUTTON 21*/
+document.addEventListener('DOMContentLoaded', function() {
+  const fadeButton = document.getElementById('Button21');
+  const myElement = document.getElementById('Line21');
+
+  fadeButton.addEventListener('click', function() {
+    Line21.style.opacity = 1; 
+  });
+
+   fadeButton.addEventListener('click', function() {
+            Button22.style.display = 'block'; 
+        });
+     fadeButton.addEventListener('click', function() {
+            Button21.style.display = 'block'; 
+        });
+});
+
+
+
+
+
