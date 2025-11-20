92a959126cc81df410365653ffad9644650dd8ca
Rylee094 <itsrylee151@gmail.com>
2025-11-19 19:22:40 -0600

    added more lines of js and code

--- museum-collection/js/bird-of-paradise.js
+++ museum-collection/js/bird-of-paradise.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $("#Button1").click(function() {
-    $("#Line1").fadeIn(); // Or .toggle(), .fadeIn(), .slideDown()
+    $("#Line1").fadeIn(); 
+    $("#Button2").show();
+  });
+});
+
+$(document).ready(function() {
+  $("#Button2").click(function() {
+    $("#Line2").fadeIn(); 
+    $("#Button3").show();
   });
 });
