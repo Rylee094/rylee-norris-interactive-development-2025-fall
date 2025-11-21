5691828b163c0f2d3b203654dfb8e3c09e66fe61
Rylee094 <itsrylee151@gmail.com>
2025-11-20 19:30:06 -0600

    set up js and jquery file

--- museum-collection/js/bird-of-paradise.js
+++ museum-collection/js/bird-of-paradise.js
@@ -1,3 +1,5 @@
+
+//BUTTON 1//
 $(document).ready(function() {
   $("#Button1").click(function() {
     $("#Line1").fadeIn(); 
@@ -5,9 +7,162 @@ $(document).ready(function() {
   });
 });
 
+//BUTTON 2//
 $(document).ready(function() {
   $("#Button2").click(function() {
     $("#Line2").fadeIn(); 
     $("#Button3").show();
+  });
+});
+
+//BUTTON 3//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 4//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 5//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 6//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 7//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 8//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 9//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 10//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 11//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 12//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 13//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 14//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 15//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 16//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 17//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 18//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 19//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 20//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 21//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
   });
 });
