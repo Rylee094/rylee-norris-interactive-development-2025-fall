aab068e333a7aa3c53b9afacadd04b8c85e13e16
Rylee094 <itsrylee151@gmail.com>
2025-11-28 15:26:18 -0600

    added pages to lobby

---
+++ museum-collection/base/base.js
@@ -0,0 +1,168 @@
+
+//BUTTON 1//
+$(document).ready(function() {
+  $("#Button1").click(function() {
+    $("#Line1").fadeIn(); 
+    $("#Button2").show();
+  });
+});
+
+//BUTTON 2//
+$(document).ready(function() {
+  $("#Button2").click(function() {
+    $("#Line2").fadeIn(); 
+    $("#Button3").show();
+  });
+});
+
+//BUTTON 3//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 4//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 5//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 6//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 7//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 8//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 9//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 10//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 11//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 12//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 13//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 14//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 15//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 16//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 17//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 18//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 19//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 20//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
+
+//BUTTON 21//
+$(document).ready(function() {
+  $("# ").click(function() {
+    $("# ").fadeIn(); 
+    $("# ").show();
+  });
+});
